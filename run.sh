5289699f48684382a3760e9fb0efad8b6b875ea2
Andrei Kozel <kozel.andrei.94@gmail.com>
2024-12-09 20:44:48 +0100

    remove unnecessary directory change in run.sh

--- run.sh
+++ run.sh
@@ -54,8 +54,6 @@ echo "::endgroup::"
 # Commit and push the changes
 echo "::group::Committing and tagging"
 echo "Current directory: $(pwd)"
-cd ..
-echo "Current directory: $(pwd)"
 git config --global user.email "kozel.andrei.94@gmail.com"
 git config --global user.name "Andrei Kozel"
 git add .
