99e30a3d90908e89f8b58839465d28662b5bf831
Andrei Kozel <kozel.andrei.94@gmail.com>
2024-12-09 21:33:22 +0100

    refactor run.sh to improve directory handling and move protoc command

--- run.sh
+++ run.sh
@@ -5,24 +5,23 @@ RELEASE_VERSION=$2
 sudo apt-get install -y protobuf-compiler golang-goprotobuf-dev
 go install google.golang.org/protobuf/cmd/protoc-gen-go@latest
 go install google.golang.org/grpc/cmd/protoc-gen-go-grpc@latest
-protoc --go_out=./golang --go_opt=paths=source_relative \
-  --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
- ./${SERVICE_NAME}/*.proto
 
-# Check if folder exists
-# If not, create it
 if [ ! -d "golang" ]; then
     mkdir golang
 fi
 cd golang
 
-# Check if folder exists
-# If not, create it
 if [ ! -d "${SERVICE_NAME}" ]; then
     mkdir ${SERVICE_NAME}
 fi
 cd ${SERVICE_NAME}
+cd ../../
 
+protoc --go_out=./golang --go_opt=paths=source_relative \
+  --go-grpc_out=./golang --go-grpc_opt=paths=source_relative \
+ ./${SERVICE_NAME}/*.proto
+
+cd golang/${SERVICE_NAME}
 go mod init \
   github.com/andrei-kozel/gladiator-arena-proto/golang/${SERVICE_NAME} ||true
 go mod tidy
